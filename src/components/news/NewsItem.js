4cf1aee7c88e0db2392b70a690905b8634e4c099
nandartika <kartikanandap@gmail.com>
2022-10-20 18:06:53 +0700

    fix(MainPage): Content position and create util time since

--- src/components/news/NewsItem.js
+++ src/components/news/NewsItem.js
@@ -7,39 +7,53 @@ import {
   Typography,
 } from "@mui/material";
 import imgPlaceholder from "../../assets/images/img-placeholder.jpeg";
+import timeSince from "../../utils/timeSince";
 
 const NewsItem = (props) => {
-  const { image, title, date, author } = props;
+  const { image, title, date, publisher } = props;
 
   return (
     <Card sx={{ maxWidth: 270, height: "100%" }}>
-      <CardActionArea>
+      <CardActionArea sx={{ height: "100%" }}>
         <CardMedia
           component="img"
           height="178"
           image={image || imgPlaceholder}
           alt={title}
         />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="div">
-            {title}
-          </Typography>
-
+        <CardContent sx={{ height: "calc(100% - 208px)" }}>
           <Grid
             container
-            direction="row"
-            justifyContent="flex-start"
-            alignItems="flex-start"
+            direction="column"
+            justifyContent="space-between"
+            alignItems="stretch"
+            sx={{ height: "100%" }}
           >
             <Grid item>
-              <Typography variant="body2" color="text.secondary">
-                {date}
+              <Typography gutterBottom variant="body1" component="div">
+                {title}
               </Typography>
             </Grid>
+
             <Grid item>
-              <Typography variant="body2" color="text.secondary">
-                {author}
-              </Typography>
+              <Grid
+                container
+                direction="row"
+                justifyContent="flex-start"
+                alignItems="flex-start"
+                spacing={2}
+              >
+                <Grid item>
+                  <Typography variant="body2" color="text.secondary">
+                    {timeSince(date)}
+                  </Typography>
+                </Grid>
+                <Grid item>
+                  <Typography variant="body2" color="text.secondary">
+                    {publisher}
+                  </Typography>
+                </Grid>
+              </Grid>
             </Grid>
           </Grid>
         </CardContent>
