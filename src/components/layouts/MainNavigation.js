9cc22c0093433239478ca58402ad5657bf950c83
nandartika <kartikanandap@gmail.com>
2022-10-20 16:36:19 +0700

    feat (MainPage): Success show hot news and latest news

---
+++ src/components/layouts/MainNavigation.js
@@ -0,0 +1,32 @@
+import { AppBar, Container, Toolbar, Typography } from "@mui/material";
+
+const MainNavigation = () => {
+  return (
+    <AppBar position="static">
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          {/* <AdbIcon sx={{ display: { xs: "none", md: "flex" }, mr: 1 }} /> */}
+          <Typography
+            variant="h6"
+            noWrap
+            component="a"
+            href="/"
+            sx={{
+              mr: 2,
+              display: { xs: "none", md: "flex" },
+              fontFamily: "monospace",
+              fontWeight: 700,
+              letterSpacing: ".3rem",
+              color: "inherit",
+              textDecoration: "none",
+            }}
+          >
+            LOGO
+          </Typography>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
+};
+
+export default MainNavigation;
