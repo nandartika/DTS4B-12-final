31e554e410e10f4747b2f60c05b886b7547341cf
nandartika <kartikanandap@gmail.com>
2022-10-20 17:16:53 +0700

    feat(MainPage): Add footer

--- src/components/layouts/Layout.js
+++ src/components/layouts/Layout.js
@@ -1,13 +1,28 @@
-import { Container } from "@mui/material";
-import { Fragment } from "react";
+import { Container, Grid } from "@mui/material";
+import Footer from "./Footer";
 import MainNavigation from "./MainNavigation";
 
 const Layout = (props) => {
   return (
-    <Fragment>
-      <MainNavigation />
-      <Container>{props.children}</Container>
-    </Fragment>
+    <Grid
+      container
+      direction="column"
+      justifyContent="center"
+      alignItems="stretch"
+      spacing={5}
+    >
+      <Grid item>
+        <MainNavigation />
+      </Grid>
+
+      <Grid item>
+        <Container>{props.children}</Container>
+      </Grid>
+
+      <Grid item>
+        <Footer />
+      </Grid>
+    </Grid>
   );
 };
 
