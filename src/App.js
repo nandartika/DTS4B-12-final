0b4fd1b89df488350d103114b233a32b4c10ba8e
kartikanandap <kartikanandap@gmail.com>
2022-10-24 23:33:50 +0700

    fix(login-signup): can't navigate to signup

--- src/App.js
+++ src/App.js
@@ -33,7 +33,7 @@ function App() {
       <Route
         path="/login"
         element={
-          <ProtectedRoute>
+          <ProtectedRoute loginOnly={false}>
             <LoginPage />
           </ProtectedRoute>
         }
@@ -41,7 +41,7 @@ function App() {
       <Route
         path="/signup"
         element={
-          <ProtectedRoute>
+          <ProtectedRoute loginOnly={false}>
             <SignUpPage />
           </ProtectedRoute>
         }
--- src/components/layouts/ProtectecRoute.js
+++ src/components/layouts/ProtectecRoute.js
@@ -4,21 +4,21 @@ import { auth } from "../../config/firebase";
 import { useEffect } from "react";
 import Progress from "./Progress";
 
-const ProtectedRoute = ({ children }) => {
+const ProtectedRoute = ({ children, loginOnly = true }) => {
   const [user, isLoading] = useAuthState(auth);
   const navigate = useNavigate();
 
   useEffect(() => {
     if (isLoading) return;
 
-    if (!user) {
+    if (!user && loginOnly) {
       navigate("/login");
     }
 
-    if (user) {
+    if (user && !loginOnly) {
       navigate("/");
     }
-  }, [user, isLoading, navigate]);
+  }, [user, isLoading, navigate, loginOnly]);
 
   return isLoading ? <Progress /> :  children;
 };
